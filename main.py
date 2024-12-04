ed7bde9859ef7ef32e405ebad0dd0991f3453e29
Waqas Khan <118829654+mwaqaskhan2002@users.noreply.github.com>
2024-12-04 11:13:30 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,15 +6,9 @@ import numpy as np
 model = joblib.load('model.pkl')
 app = FastAPI()
 
-class PredictRequest(BaseModel):
-    sepal_length: float
-    sepal_width: float
-    petal_length: float
-    petal_width: float
-    
 @app.post('/predict')
-def predict(request:PredictRequest):
-    data = np.array([[request.sepal_length, request.sepal_width, request.petal_length, request.petal_width]])
+def predict(sepal_length: float, sepal_width: float, petal_length: float, petal_width: float):
+    data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
     prediction = model.predict(data)
     species_map = {0: "setosa", 1: "versicolor",2: "virginica"}
     return {"prediction": species_map[int(prediction[0])]}
